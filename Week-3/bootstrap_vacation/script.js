a1fb3b3e1d318c0c2403459f19921c4170220ccb
Abbas <meerabbas110@gmail.com>
2023-08-19 22:05:42 +0530

    added pop up contact form from footer

--- Week-3/bootstrap_vacation/script.js
+++ Week-3/bootstrap_vacation/script.js
@@ -1,12 +1,17 @@
-function showAlert(alertId) {
-    const bootstrapAlert = document.querySelector("#" + alertId);
-    const collapse = new bootstrap.Collapse(bootstrapAlert); 
-    collapse.show();
- }
+
+ // When the page is loaded
+document.addEventListener("DOMContentLoaded", function () {
+   // Show the Modal when the "Send a message" button is clicked
+   document.getElementById("showModalBtn").addEventListener("click", function () {
+     $("#contact-modal").modal("show");
+   });
  
- const modalYesBtn = document.querySelector("#contact-modal .btn-primary");
- modalYesBtn.addEventListener("click", function () {    
-    const sendBtn = document.querySelector("#send-message-btn");
-    sendBtn.style.display = "none";
-    showAlert("conf-alert");
+   // When the "Send message" button in the Modal is clicked
+   document.getElementById("sendMessageBtn").addEventListener("click", function () {
+     // Hide the Modal
+     $("#contact-modal").modal("hide");
+     // Show the Alert
+     document.getElementById("conf-alert").classList.remove("collapse");
+   });
  });
+ 
