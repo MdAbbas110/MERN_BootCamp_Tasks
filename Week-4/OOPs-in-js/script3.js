63505c59c5ad6ff3e60440e9a5c58414f95a47cd
Abbas <meerabbas110@gmail.com>
2023-09-04 16:42:40 +0530

    second solution of the same problem

--- Week-4/OOPs-in-js/script3.js
+++ Week-4/OOPs-in-js/script3.js
@@ -23,3 +23,33 @@ Object.defineProperties(person, 'age', {
     enumerable: true,
     configurable: false
 })
+
+
+// second part of the question where use of prototype
+
+class Vehicle  {
+    constructor(make, model, year){
+        this.make = make
+        this.model = model
+        this.year = year
+    }
+    getDetails() {
+        return `Make: ${this.make} and Model is ${this.model} and year ${this.year}`
+    }
+}
+
+class Car extends Vehicle {
+    constructor(make, model, year, numDoors){
+        super(make, model, year)
+        this.numDoors = numDoors
+    }
+    getDetails() {
+        return `${super.getDetails()} Number of Doors are ${this.numDoors}`
+    }
+}
+
+const vehicle = new Vehicle('Honda','i20', 2018)
+console.log(vehicle.getDetails());
+
+const car = new Car('toyota','Cardio',2022, 8)
+console.log(car.getDetails());
