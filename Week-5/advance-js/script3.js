f8b7be1b1e9f01e6aa1d72d8bc026451c33bc3b3
Abbas <meerabbas110@gmail.com>
2023-09-05 23:26:30 +0530

    added the symbol( ) to generate new transition id

---
+++ Week-5/advance-js/script3.js
@@ -0,0 +1,11 @@
+let counter = 1
+
+function generateTransactionId (){
+    let newTran = counter++
+
+    const str = `Transition_ID_${newTran}`
+
+    const unique = Symbol(str)
+
+    return unique
+}
